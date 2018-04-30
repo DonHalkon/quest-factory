78ea3eba2b0f1ba63f752efbd606c753d126e7f2
Sviatoslav Naiden <sv.nayden@gmail.com>
2018-04-30 11:31:35 +0300

    init

---
+++ src/main/java/com/naiden/questfactory/QuestFactoryApplication.java
@@ -0,0 +1,12 @@
+package com.naiden.questfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestFactoryApplication.class, args);
+	}
+}
